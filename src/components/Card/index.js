6cc1fd8b7a9e39829c3bd047b6b2cffa3807dc7c
Guilherme Sanches <guitdsanches@gmail.com>
2023-05-22 12:26:41 -0300

    adding favorites context

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,17 @@
+import { useFavoritoContext } from "contextos/Favoritos";
 import styles from "./Card.module.css";
 import iconeFavoritar from "./favoritar.png";
 
 
 const Card = ({ id, titulo, capa }) => {
-    return ( 
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    return (
         <div className={styles.container}>
-            <img src={capa} alt={titulo}/>
+            <img src={capa} alt={titulo} />
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar} alt="Favoritar filme" className={styles.favoritar}/>
+            <img src={iconeFavoritar} alt="Favoritar filme" className={styles.favoritar} onClick={() => adicionarFavorito({ id, titulo, capa })} />
         </div>
-     );
+    );
 }
- 
+
 export default Card;
